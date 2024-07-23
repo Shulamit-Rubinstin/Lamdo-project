5ecd39b211cf031441d2ec7f4993c236d73d0d34
Shulamit-Rubinstin <yr8410@gmail.com>
2024-07-23 15:03:15 +0300

    smallForm

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms'; // הוסף כאן
+import{SignatureComponent}from '../signature/signature.component'
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,SignatureComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -18,7 +19,14 @@ export class FormComponent implements OnInit{
       permission:['',[Validators.required]],
       fromKosher:['',[Validators.required]],
       toKosher:['',[Validators.required]],
-      
+      fulName: ['', [Validators.required]],
+      class:['', [Validators.required]],
+      parentsName:['',[Validators.required]],
+      permissionF:['',[Validators.required]],
+      toKosherF:['',[Validators.required]],
+      institutionNameF:['',[Validators.required]],
+      fromKosherF:['',[Validators.required]],
+      date:[Date,[Validators.required]],
     });
   }
 }
